bb1dac59a7ec2c3f00a7fa532ad2024e09541749
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-09-25 14:23:18 +0300

    Completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
+
 import React from "react";
 
-function Article() {
-  return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
-}
+const Article = () => <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+ 
+  
+  
 
 export default Article;
--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
-}
+const Comment = () => <div>Naturally, I agree with this article.</div>;
+
 
 export default Comment;
